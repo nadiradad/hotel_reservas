2d615bf0d8aafe173d9902134e2604f3309104be
Gastón Ariel Perrone <gastonarielp1812@gmail.com>
2025-09-25 20:10:24 -0300

    admin.py is already filters into class ClienteAdmin with decoration register about admin.

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Cliente
 
 # Register your models here.
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'email', 'telefono', 'dni', 'creado_en')
+    search_fields = ('nombre', 'apellido', 'email', 'dni')
+    list_filter = ('creado_en',)
+    ordering = ('apellido', 'nombre')
