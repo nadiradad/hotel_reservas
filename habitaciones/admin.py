8c1011662239d32a046fd79f5e767df54be360c3
Gastón Ariel Perrone <gastonarielp1812@gmail.com>
2025-09-26 17:12:17 -0300

    admin.py about Habitación and TipoHabitacion is added for admin filters and searchs.

--- habitaciones/admin.py
+++ habitaciones/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import TipoHabitacion, Habitacion
 
 # Register your models here.
+@admin.register(TipoHabitacion)
+class TipoHabitacionAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio_noche', 'capacidad')
+    search_fields = ('nombre',)
+    list_filter = ('capacidad',)
+    
+@admin.register(Habitacion)
+class HabitacionAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'tipo', 'activa')
+    search_fields = ('numero',)
+    list_filter = ('tipo', 'activa')
