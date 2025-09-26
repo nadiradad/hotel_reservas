8e3d96ff4d8216f427f71f5c96fcc6c2370b3ba4
Gastón Ariel Perrone <gastonarielp1812@gmail.com>
2025-09-26 17:27:50 -0300

    admin.py about Pago is already.

--- pagos/admin.py
+++ pagos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Pago
 
 # Register your models here.
+@admin.register(Pago)
+class PagoAdmin(admin.ModelAdmin):   
+    list_display = ('reserva', 'monto', 'metodo', 'estado', 'fecha_pago', 'creado_en')
+    list_filter = ('metodo', 'estado', 'fecha_pago')
+    search_fields = ('reserva__id', 'referencia')
+    ordering = ('fecha_pago',)
